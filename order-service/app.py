6a59f4f56d7c048125ae72084a574387c7bc4026
TejeshKumarReddyTR <TejeshKumarReddy.C@thomsonreuters.com>
2025-07-14 12:35:38 +0530

    v2.17

--- order-service/app.py
+++ order-service/app.py
@@ -16,7 +16,8 @@ def place_order():
 @app.route('/order/orders', methods=['GET'])
 def list_orders():
     print(f"Queue URL: {queue_url}")
-    return jsonify([{"id": 1, "product_id": 1, "status": "confirmed"}])
+    return queue_url
+    #return jsonify([{"id": 1, "product_id": 1, "status": "confirmed"}])
 @app.route('/health', methods=['GET'])
 def health():
     return "Order service healthy", 200
