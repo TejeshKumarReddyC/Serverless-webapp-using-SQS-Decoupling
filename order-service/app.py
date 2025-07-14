29054f633f0a0261d0657d1b685278cabd692062
TejeshKumarReddyTR <TejeshKumarReddy.C@thomsonreuters.com>
2025-07-14 12:57:04 +0530

    v2.18

--- order-service/app.py
+++ order-service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import boto3, os, json
+import logging
 
 app = Flask(__name__)
 
@@ -8,10 +9,24 @@ queue_url = os.environ.get("ORDER_QUEUE_URL", "")
 
 @app.route('/order/order', methods=['POST'])
 def place_order():
-    #print(f"Queue URL: {queue_url}")
-    data = request.get_json()
-    sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(data))
-    return jsonify({"message": "Order placed successfully"}), 201
+    
+    try:
+        data = request.get_json()
+        logging.info(f"Received order: {data}")
+        logging.info(f"Queue URL: {queue_url}")
+        
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps(data)
+        )
+        
+        logging.info(f"SQS response: {response}")
+        return jsonify({"message": "Order placed successfully"}), 201
+
+    except Exception as e:
+        logging.error("Exception occurred while placing order", exc_info=True)
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/order/orders', methods=['GET'])
 def list_orders():
