b4e870ec99aaea1eeabfe64adfd9e919e6fe70a6
TejeshKumarReddyTR <TejeshKumarReddy.C@thomsonreuters.com>
2025-07-14 12:14:11 +0530

    v2.15

--- order-service/app.py
+++ order-service/app.py
@@ -8,6 +8,7 @@ queue_url = os.environ.get("ORDER_QUEUE_URL", "")
 
 @app.route('/order/order', methods=['POST'])
 def place_order():
+    #print(f"Queue URL: {queue_url}")
     data = request.get_json()
     sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(data))
     return jsonify({"message": "Order placed successfully"}), 201
@@ -15,7 +16,7 @@ def place_order():
 @app.route('/order/orders', methods=['GET'])
 def list_orders():
     return jsonify([{"id": 1, "product_id": 1, "status": "confirmed"}])
-
+    print(f"Queue URL: {queue_url}")
 @app.route('/health', methods=['GET'])
 def health():
     return "Order service healthy", 200
